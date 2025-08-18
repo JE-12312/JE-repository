d6284023a46415e17f2e9b6f1b89f066ef4c228d
JE-12312 <jaeun4137@naver.com>
2025-08-18 17:01:07 +0900

    Update streamlit_rag_memory-2.py

--- streamlit_rag_memory-2.py
+++ streamlit_rag_memory-2.py
@@ -14,6 +14,17 @@ from langchain_core.output_parsers import StrOutputParser
 import chromadb
 chromadb.api.client.SharedSystemClient.clear_system_cache()
 
+# Chroma 배포 시 sqlite3 관련 의존성 문제 해결 코드
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+# API key 탈취 우려 있으므로 보안조치
+from langchain_chroma import Chroma
+os.environ["OPENAI_API_KEY"] = st.secrets['OPENAI_API_KEY']
+
+
+
 
 #cache_resource로 한번 실행한 결과 캐싱해두기
 @st.cache_resource
@@ -93,3 +104,4 @@ if uploaded_file is not None:
                 st.session_state.messages.append({"role": "assistant", "content": response})
                 st.write(response)
                 
+
